d347c3b06f5e25fc34c1611bbba2601333ec7f71
lucassodresa <lucas.sodresa@gmail.com>
2022-07-16 14:07:37 +0100

    pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 const UserList = () => {
@@ -129,6 +130,7 @@ const UserList = () => {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
